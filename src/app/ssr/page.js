2d05d68bc63ab79646fc0c36149f45180fde57ca
izumix03 <takahiro.izumikawa@gmail.com>
2023-06-28 12:42:25 +0900

    first commit

---
+++ src/app/ssr/page.js
@@ -0,0 +1,26 @@
+export default async function SSR( ) {
+    const formattedDate = await date();
+    return (
+        <>
+            <h1>Server-side rendered page</h1>
+            <p>
+                This page is server-side rendered. It was rendered on {formattedDate}.
+            </p>
+            <p>
+                <a href="/">View a static page.</a>
+            </p>
+        </>
+    );
+}
+
+ async function date() {
+    const renderDate = Date.now();
+    const formattedDate = new Intl.DateTimeFormat("en-US", {
+        dateStyle: "long",
+        timeStyle: "long",
+    }).format(renderDate);
+    console.log(
+        `SSR ran on ${formattedDate}. This will be logged in CloudWatch.`
+    );
+    return formattedDate;
+}
